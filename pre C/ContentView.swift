5d2ff1e1c8195fd663ff03da59abb63f9c1cf216
Yuuto <2021m014@kuas.ac.jp>
2023-09-27 10:38:06 +0900

    Initial Commit

---
+++ pre C/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  pre C
+//
+//  Created by 千葉勇斗 on 2023/09/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ pre C/pre_CApp.swift
@@ -0,0 +1,17 @@
+//
+//  pre_CApp.swift
+//  pre C
+//
+//  Created by 千葉勇斗 on 2023/09/27.
+//
+
+import SwiftUI
+
+@main
+struct pre_CApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
